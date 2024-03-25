8af60add7de18bbc71ff79767bc1cd5b98dc5e78
Samuele Pecetto <samuele.pecetto@newday.co.uk>
2024-03-25 09:29:35 +0000

    refactor: solution structure

---
+++ Katas/Shared/Clock.cs
@@ -0,0 +1,55 @@
+﻿namespace NewDay.Katas.Shared;
+
+public class Clock : IDisposable
+{
+    private static DateTime? _startTime;
+    private static DateTime? _nowForTest;
+
+    public static DateTime Now
+    {
+        get
+        { 
+            if (_nowForTest == null)
+            {
+                return DateTime.Now;
+            }
+            else
+            {
+                //freeze time
+                if (_startTime == null)
+                {
+                    return _nowForTest.GetValueOrDefault();
+                }
+                //keep running
+                else
+                {
+                    TimeSpan elapsedTime = DateTime.Now.Subtract(_startTime.GetValueOrDefault());
+                    return _nowForTest.GetValueOrDefault().Add(elapsedTime);
+                }
+            }
+        }
+    }
+
+    public static IDisposable NowIs(DateTime dateTime, bool keepTimeRunning = false)
+    {
+        _nowForTest = dateTime;
+        if (keepTimeRunning)
+        {
+            _startTime = DateTime.Now;
+        }
+        return new Clock();
+    }
+
+    public static IDisposable ResetNowIs()
+    {
+        _startTime = null;
+        _nowForTest = null;
+        return new Clock();
+    }
+
+    public void Dispose()
+    {
+        _startTime = null;
+        _nowForTest = null;
+    }
+}
---
+++ Katas/Throttling/ThrottledExecutor.cs
@@ -0,0 +1,25 @@
+﻿namespace NewDay.Katas.Throttling;
+
+public class ThrottledExecutor(int threshold)
+{
+    // think about how to abstract the "time" so we don't have to
+    // really wait for X seconds to complete a test
+    public void Invoke(Action lambda)
+    {
+        CheckAndDecreaseThreshold();
+        lambda.Invoke();
+    }
+
+    private void CheckAndDecreaseThreshold()
+    {
+        if (threshold <= 0)
+            throw new ApplicationException("Can't accept requests rn");
+        threshold--;
+    }
+
+    public T Invoke<T>(Func<T> lambda)
+    {
+        CheckAndDecreaseThreshold();
+        return lambda.Invoke();
+    }
+}
